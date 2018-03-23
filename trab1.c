51dd3bafec0f98d754445dc5027ab9976cee08bb
Augusto César Bisognin <augustocb@gmail.com>
2018-03-23 18:21:39 -0300

    Corrigidas validações

--- trab1.c
+++ trab1.c
@@ -18,13 +18,19 @@
  * 
   */
 
-/*estrutura de um arquivo*/
+/* estrutura de um arquivo */
 typedef struct img {
-    File* arquivo;
+    FILE* arquivo;
     char* nome;
     int alt;
     int larg;
 } Imagem;
+/* estrutura de um pixel */
+typedef struct pxl {
+    int r;
+    int g;
+    int b;
+} Pixel;
 
 /*declarações das funções secundárias*/
 char* cria_string(char* palavra);
@@ -34,27 +40,31 @@ int main( int argc, char *argv[]){
     setlocale (LC_ALL, "portuguese"); /*Define a codificação*/
     
     /* testa os argumentos */
-    if (argc == 1){
+    bool erro_param = false; /* não informou nenhum parâmetro */
+    if (argc == 1)
+        erro_param=true;
+    if (*argv[1] == '-' || *argv[1] == '\\') /* verifica se o primeiro parâmetro pode ser arquivo */
+        erro_param=true;
+
+    if (erro_param){
         /* verifica se o primeiro argumento é um parâmetro (inicia com - ou \) */
 		printf("Use %s [SAÍDA] [OPÇÕES]\n", argv[0]);
-        if (argv[1] == '-' || argv[1] == '\\') {
-            printf("O primeiro argumento deve ser o nome arquivo de saída\n\n")
-        } else printf("\n");
+        printf("O primeiro argumento deve ser o nome arquivo de saída\n\n");
         
 		printf("As opções podem ser:\n");
 		exit(1);
 	}
 
     /* abre o arquivo */
-    Imagem imagem*;
+    Imagem* imagem;
     imagem = malloc(sizeof(Imagem));
     if (imagem == NULL){
         printf("Memória insuficiente!\n\tLibere mais memória e tente novamente.\n", argv[1]);
         exit(5);
     }
-    imagem->arquivo = fopen(argv[1], w+);
+    imagem->arquivo = fopen(argv[1], "r+");
     if (imagem->arquivo == NULL){
-        printf("Falha ao abrir o arquivo %d", argv[1]);
+        printf("Falha ao abrir o arquivo %s", argv[1]);
         exit(2);
     }
     imagem->nome=cria_string(argv[1]); /* salva o nome do arquivo */
@@ -62,13 +72,14 @@ int main( int argc, char *argv[]){
     bool arq_valido = true;
     char tipo[5];
     fgets(tipo, 5, imagem->arquivo);
-    if (strlen(tipo) != 2 || tipo[0] != 'P' || !isdigit(tipo[1]) )
+    if (strlen(tipo) != 3 || (char)tipo[0] != 'P' || isdigit(tipo[1]) )
         arq_valido = false;
     if (!arq_valido){
         printf("Formato de arquivo inválido\n");
-        printf("%d\n\n", tipo);
+        printf("Tipo: %c%c\t(%d)\n\n", (char)tipo[0],(char)tipo[1], strlen(tipo));
         exit(3);
     }
+    printf("Cabeçalho do arquivo %s lido com sucesso",imagem->nome);
 
     //TODO testa as dimensões exit(4)
 
